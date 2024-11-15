335010b1e5ced95e0c6240ad89329cdc52f80b7b
Jamshid Masharipov <masharipovjamshid569@gmail.com>
2024-11-15 14:35:17 +0500

    I created the class and function calling from Template files

--- my_tennis_club/members/urls.py
+++ my_tennis_club/members/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import hello
+from .views import hello,about,Home_Viev
 
 
 urlpatterns=[
     path('', hello, name='hello'),
+    path('about/', about, name='about'),
+    path('home/', Home_Viev.as_view(), name='home'),
 
 ]
--- my_tennis_club/members/views.py
+++ my_tennis_club/members/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from django.views.generic import TemplateView
+
+
+
 
 def  hello (request):
     return HttpResponse('Hello World !')
+
+def about(request):
+    '''  calling from def function this  template files '''
+    template = loader.get_template('about.html')
+    return HttpResponse(template.render())
+
+class Home_Viev(TemplateView):
+    template_name = 'home.html'
+
 
 
 
