28973b8eb5ee5604d91e63b9ac8d68dc53798ef7
Jamshid Masharipov <masharipovjamshid569@gmail.com>
2024-11-18 16:01:16 +0500

    Db config

--- my_tennis_club/config_my_tennis/settings.py
+++ my_tennis_club/config_my_tennis/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,7 +78,11 @@ WSGI_APPLICATION = 'config_my_tennis.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.getenv('db_name'),
+        'USER': os.getenv('db_user'),
+        'PASSWORD': os.getenv('password'),
+        'HOST': os.getenv('host'),
+        'PORT': os.getenv('port'),
     }
 }
 
